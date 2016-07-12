810024bc0f1fb33cdb5902d82fd37e46c7d77488
David Goldwasser <david.f.goldwasser@gmail.com>
2016-07-12 14:01:58 -0600

    copying resource changes to other measures

--- measures/bestest_space_cooling_equipment_performance_tests/resources/besttest_model_methods.rb
+++ measures/bestest_space_cooling_equipment_performance_tests/resources/besttest_model_methods.rb
@@ -57,4 +57,137 @@ module BestestModelMethods
 
   end
 
+  # add options to method for variables that change across cases
+  def self.create_he_system(runner,model,variable_hash)
+
+    # BESTEST he system
+    # This measure creates:
+    # creates an air loop with heating only and single zone setpoint manager
+    # there is no outdoor air
+    # todo - has ciriculating and draft fan
+
+    # create always on schedule
+    always_on = model.alwaysOnDiscreteSchedule
+
+    air_flow_rate = 0.355
+
+    # get the only zone in the model
+    zone = model.getThermalZones.first
+
+    # Add air loop
+    air_loop = OpenStudio::Model::AirLoopHVAC.new(model)
+    air_loop.setName("BESTEST HE air loop")
+    air_loop.setDesignSupplyAirFlowRate(air_flow_rate)
+    runner.registerInfo("HVAC > Adding airloop named #{air_loop.name}")
+
+    # don't know if I need to edit any air loop sizing properties
+    air_loop_sizing = air_loop.sizingSystem #
+
+    # todo - idf uses AirLoopHVAC:Unitary:Furnace:HeatOnly. Do I need ot use that?
+
+    # curve for heating coil
+    furnace_pldf_curve_default = OpenStudio::Model::CurveCubic.new(model)
+    if variable_hash[:plr] == 1.0
+      furnace_pldf_curve_default.setCoefficient1Constant(1.0)
+      furnace_pldf_curve_default.setCoefficient2x(0.0)
+      furnace_pldf_curve_default.setCoefficient3xPOW2(0.0)
+      furnace_pldf_curve_default.setCoefficient4xPOW3(0.0)
+      furnace_pldf_curve_default.setMinimumValueofx(0.0)
+      furnace_pldf_curve_default.setMaximumValueofx(1.0)
+    elsif variable_hash[:plr] == 0.4
+      furnace_pldf_curve_default.setCoefficient1Constant(1.005519483)
+      furnace_pldf_curve_default.setCoefficient2x(0.182253228)
+      furnace_pldf_curve_default.setCoefficient3xPOW2(-0.527668)
+      furnace_pldf_curve_default.setCoefficient4xPOW3(0.339946081)
+      furnace_pldf_curve_default.setMinimumValueofx(0.0)
+      furnace_pldf_curve_default.setMaximumValueofx(1.0)
+    elsif variable_hash[:plr] == 0
+      # todo - why does 0.0 have same curve as 1.0
+      furnace_pldf_curve_default.setCoefficient1Constant(1.0)
+      furnace_pldf_curve_default.setCoefficient2x(0.0)
+      furnace_pldf_curve_default.setCoefficient3xPOW2(0.0)
+      furnace_pldf_curve_default.setCoefficient4xPOW3(0.0)
+      furnace_pldf_curve_default.setMinimumValueofx(0.0)
+      furnace_pldf_curve_default.setMaximumValueofx(1.0)
+    elsif variable_hash[:plr] == [0.0,0.8]
+      # todo - why does this have same curve as 0.4
+      furnace_pldf_curve_default.setCoefficient1Constant(1.005519483)
+      furnace_pldf_curve_default.setCoefficient2x(0.182253228)
+      furnace_pldf_curve_default.setCoefficient3xPOW2(-0.527668)
+      furnace_pldf_curve_default.setCoefficient4xPOW3(0.339946081)
+      furnace_pldf_curve_default.setMinimumValueofx(0.0)
+      furnace_pldf_curve_default.setMaximumValueofx(1.0)
+    elsif variable_hash[:plr] == [0.0,1.0]
+      # todo - why does this have same curve as 0.4
+      furnace_pldf_curve_default.setCoefficient1Constant(1.005519483)
+      furnace_pldf_curve_default.setCoefficient2x(0.182253228)
+      furnace_pldf_curve_default.setCoefficient3xPOW2(-0.527668)
+      furnace_pldf_curve_default.setCoefficient4xPOW3(0.339946081)
+      furnace_pldf_curve_default.setMinimumValueofx(0.0)
+      furnace_pldf_curve_default.setMaximumValueofx(1.0)
+    else
+      runner.registerError("Unexpected plr variable value")
+      returnf false
+    end
+
+    # Add heating coil
+    htg_coil = OpenStudio::Model::CoilHeatingGas.new(model,always_on)
+    htg_coil.setGasBurnerEfficiency(variable_hash[:ss_eff] / 100.0 )
+    htg_coil.setNominalCapacity(variable_hash[:capacity] * 1000.0)
+    htg_coil.setPartLoadFractionCorrelationCurve(furnace_pldf_curve_default)
+    if variable_hash[:draft_fan_power] > 0.0
+      # todo - is this inducing heat where we don't want it and is it cycling how we want it to
+      htg_coil.setParasiticElectricLoad(variable_hash[:draft_fan_power])
+    end
+
+    # todo - FanOnOff doesn't seem to work here
+    fan = OpenStudio::Model::FanConstantVolume.new(model,always_on)
+    fan.setMaximumFlowRate(air_flow_rate)
+    fan.setMotorInAirstreamFraction(0.0)
+    if variable_hash[:circ_fan_power] == 0
+      fan.setFanEfficiency(1.0)
+      fan.setPressureRise(0.0)
+      fan.setMotorEfficiency(1.0)
+    elsif variable_hash[:circ_fan_power] == 200.0 and variable_hash[:circ_fan_type] == "cont"
+      fan.setFanEfficiency(0.441975)
+      fan.setPressureRise(249.0)
+      fan.setMotorEfficiency(0.441975)
+    elsif variable_hash[:circ_fan_power] == 200.0 and variable_hash[:circ_fan_type] == "cyclic"
+      fan.setFanEfficiency(0.441975)
+      fan.setPressureRise(249.0)
+      fan.setMotorEfficiency(0.9)
+    else
+      runner.registerError("Unexpected circulating fan variable values")
+      returnf false
+    end
+
+    # Add outdoor air (is this in unitary in IDF?)
+    # todo - this doesn't add in any oa since zones not requesting it, but can air loop still add some when heating
+    oa_controller = OpenStudio::Model::ControllerOutdoorAir.new(model)
+    oa_controller.setMinimumOutdoorAirFlowRate(air_flow_rate)
+    oa_controller.setMaximumOutdoorAirFlowRate(air_flow_rate)
+    oa_system = OpenStudio::Model::AirLoopHVACOutdoorAirSystem.new(model,oa_controller)
+
+    # Add the components to the air loop
+    # in order from closest to zone to furthest from zone
+    supply_inlet_node = air_loop.supplyInletNode
+    htg_coil.addToNode(supply_inlet_node)
+    fan.addToNode(supply_inlet_node)
+    oa_system.addToNode(supply_inlet_node)
+
+    # Add a setpoint manager single zone reheat to control the
+    # supply air temperature based on the needs of this zone
+    setpoint_mgr_single_zone_reheat = OpenStudio::Model::SetpointManagerSingleZoneReheat.new(model)
+    setpoint_mgr_single_zone_reheat.setControlZone(zone)
+    setpoint_mgr_single_zone_reheat.addToNode(air_loop.supplyOutletNode)
+
+    # Create a diffuser and attach the zone/diffuser pair to the air loop
+    diffuser = OpenStudio::Model::AirTerminalSingleDuctUncontrolled.new(model,always_on)
+    diffuser.setMaximumAirFlowRate(air_flow_rate)
+    air_loop.addBranchForZone(zone,diffuser.to_StraightComponent)
+
+    return air_loop
+
+  end
+
 end
