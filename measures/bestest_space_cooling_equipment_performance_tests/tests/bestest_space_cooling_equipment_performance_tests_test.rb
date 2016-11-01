215c1fe4bba5b2bdb5b736430fc6d0e964992a95
David Goldwasser <david.f.goldwasser@gmail.com>
2016-11-01 10:57:27 -0600

    minor change to off season infil for 340
    It was set to 15% but should have been 0

--- measures/bestest_space_cooling_equipment_performance_tests/tests/bestest_space_cooling_equipment_performance_tests_test.rb
+++ measures/bestest_space_cooling_equipment_performance_tests/tests/bestest_space_cooling_equipment_performance_tests_test.rb
@@ -226,6 +226,120 @@ class BESTESTSpaceCoolingEquipmentPerformanceTestsTest < MiniTest::Unit::TestCas
     model.save(output_file_path,true)
   end
 
+  def test_case_ce_330
+    # create an instance of the measure
+    measure = BESTESTSpaceCoolingEquipmentPerformanceTests.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+
+    # load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/seed_empty.osm")
+    model = translator.loadModel(path)
+    assert((not model.empty?))
+    model = model.get
+
+    # get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
+
+    # create hash of argument values.
+    # If the argument has a default that you want to use, you don't need it in the hash
+    args_hash = {}
+    args_hash["case_num"] = "CE330 - Outside Air"
+    # using defaults values from measure.rb for other arguments
+
+    # populate argument with specified hash value if specified
+    arguments.each do |arg|
+      temp_arg_var = arg.clone
+      if args_hash[arg.name]
+        assert(temp_arg_var.setValue(args_hash[arg.name]))
+      end
+      argument_map[arg.name] = temp_arg_var
+    end
+
+    # run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+
+    # show the output
+    show_output(result)
+
+    # assert that it ran correctly
+    assert_equal("Success", result.value.valueName)
+    #assert(result.info.size == 1)
+    #assert(result.warnings.size == 0)
+
+    # save the model to test output directory
+    output_file_path = OpenStudio::Path.new(File.dirname(__FILE__) + "/output/CE330_test_output.osm")
+    model.save(output_file_path,true)
+
+    # forward translate OSM file to IDF file
+    ft = OpenStudio::EnergyPlus::ForwardTranslator.new
+    workspace = ft.translateModel(model)
+    output_file_path_idf = OpenStudio::Path.new(File.dirname(__FILE__) + "/output/CE330_test_output.idf")
+    workspace.save(output_file_path_idf,true)
+
+  end
+
+  def test_case_ce_430
+    # create an instance of the measure
+    measure = BESTESTSpaceCoolingEquipmentPerformanceTests.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Ruleset::OSRunner.new
+
+    # load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/seed_empty.osm")
+    model = translator.loadModel(path)
+    assert((not model.empty?))
+    model = model.get
+
+    # get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
+
+    # create hash of argument values.
+    # If the argument has a default that you want to use, you don't need it in the hash
+    args_hash = {}
+    args_hash["case_num"] = "CE340 - Infil/OA Interaction"
+    # using defaults values from measure.rb for other arguments
+
+    # populate argument with specified hash value if specified
+    arguments.each do |arg|
+      temp_arg_var = arg.clone
+      if args_hash[arg.name]
+        assert(temp_arg_var.setValue(args_hash[arg.name]))
+      end
+      argument_map[arg.name] = temp_arg_var
+    end
+
+    # run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+
+    # show the output
+    show_output(result)
+
+    # assert that it ran correctly
+    assert_equal("Success", result.value.valueName)
+    #assert(result.info.size == 1)
+    #assert(result.warnings.size == 0)
+
+    # save the model to test output directory
+    output_file_path = OpenStudio::Path.new(File.dirname(__FILE__) + "/output/CE340_test_output.osm")
+    model.save(output_file_path,true)
+
+    # forward translate OSM file to IDF file
+    ft = OpenStudio::EnergyPlus::ForwardTranslator.new
+    workspace = ft.translateModel(model)
+    output_file_path_idf = OpenStudio::Path.new(File.dirname(__FILE__) + "/output/CE340_test_output.idf")
+    workspace.save(output_file_path_idf,true)
+
+  end
+
   def test_case_ce_360
     # create an instance of the measure
     measure = BESTESTSpaceCoolingEquipmentPerformanceTests.new
