94886d47277fc4cc548bab75d6d72d8bd49afc7d
David Goldwasser <david.goldwasser@nrel.gov>
2020-03-25 12:58:14 -0600

    updating common info with tentative dates

--- results/resources/common_info.rb
+++ results/resources/common_info.rb
@@ -6,16 +6,16 @@ module BestestResults
     hash = {}
 
     if program == "OS"
-      hash[:program_name_and_version] = "OpenStudio 2.9.0"
-      hash[:program_version_release_date] = "10/11/2019"
+      hash[:program_name_and_version] = "OpenStudio 3.0.0"
+      hash[:program_version_release_date] = "03/31/2020"
       hash[:program_name_short] = "OS"
     else
-      hash[:program_name_and_version] = "EnergyPlus 9.2.0"
-      hash[:program_version_release_date] = "9/27/2019"
+      hash[:program_name_and_version] = "EnergyPlus 9.3.0"
+      hash[:program_version_release_date] = "03/31/2020"
       hash[:program_name_short] = "E+"
     end
 
-    hash[:results_submission_date] = "12/11/2019"
+    hash[:results_submission_date] = "03/31/2020"
     hash[:organization] = "National Renewable Energy Laboratory"
     hash[:organization_short] = "NREL"
 
